8b0ce48b7d3abe4fcd045ac4328d30de368d36e0
Noah <nkr2vr@virginia.edu>
2018-01-08 16:31:38 -0500

    renamed folder Interrupts and Timers

---
+++ Interrupts and Timers/Part b - PortInterrupts/timerA.h
@@ -0,0 +1,8 @@
+#ifndef TIMERA_H
+#define TIMERA_H
+
+#include "msp.h"
+
+void TimeDelay(unsigned int delay, unsigned int resolution);
+
+#endif
--- Interrupts-and-Timers/Part b - PortInterrupts/timerA.h
+++
@@ -1,8 +0,0 @@
-#ifndef TIMERA_H
-#define TIMERA_H
-
-#include "msp.h"
-
-void TimeDelay(unsigned int delay, unsigned int resolution);
-
-#endif
